82caf17d8f3cfcd0c8381f4c5af070fc7ee7bf43
Rafael Dzhancherov <bokettodev@gmail.com>
2024-01-02 22:35:43 +0300

    chore: common improvements

--- src/app/shared/functions/shuffle-array.function.ts
+++ src/app/shared/functions/shuffle-array.function.ts
@@ -1,5 +1,5 @@
 export function shuffleArray<T>(array: T[]): T[] {
-  array = [...array]
+  array = [...array];
   let currentIndex = array.length;
 
   // While there remain elements to shuffle.
@@ -9,7 +9,10 @@ export function shuffleArray<T>(array: T[]): T[] {
     currentIndex -= 1;
 
     // And swap it with the current element.
-    [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
   }
 
   return array;
