00eab7258a268e828c73df2b361461d865ba9d7b
Rafael Dzhancherov <bokettodev@gmail.com>
2024-01-02 23:20:11 +0300

    chore: add TaigaUI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,27 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {TypingSpeedModule} from '@modules/typing-speed';
-import {AppComponent} from './app.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TypingSpeedModule } from '@modules/typing-speed';
+import {
+  TUI_SANITIZER,
+  TuiAlertModule,
+  TuiDialogModule,
+  TuiRootModule,
+} from '@taiga-ui/core';
+import { NgDompurifySanitizer } from '@tinkoff/ng-dompurify';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, TypingSpeedModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    TypingSpeedModule,
+    TuiRootModule,
+    TuiDialogModule,
+    TuiAlertModule,
+  ],
   bootstrap: [AppComponent],
+  providers: [{ provide: TUI_SANITIZER, useClass: NgDompurifySanitizer }],
 })
-export class AppModule {
-}
+export class AppModule {}
